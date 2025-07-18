96e7b7bd60cf35e678830c04296bef94ac162148
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-07-17 20:04:03 -0500

    Correccion en la dependencia de mailer

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.DB_HOST,
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe());
