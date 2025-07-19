7d0b68b5b901af469583930b294eb946f3a3103b
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-07-18 20:24:27 -0500

    Configuracion de cors para que funcione con vercel

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.DB_HOST,
+
+  const isProduction = process.env.NODE_ENV === 'production';
+
+app.enableCors({
+    origin: isProduction ? process.env.DB_HOST_VERCEL : process.env.DB_HOST,
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe());
