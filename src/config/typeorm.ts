b100f637a116e21e9577d22f1ea2800385c92b75
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-07-18 20:03:20 -0500

    Configuracion typeorm para que funcione ya sea local o por RENDER

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,17 +1,29 @@
 import { registerAs } from "@nestjs/config";
 import { DataSource, DataSourceOptions } from "typeorm";
 
-export default registerAs('typeorm', () => ({
-    type: 'postgres',
-    database: process.env.DB_NAME || process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT ?? '', 10) || 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    autoLoadEntities: true,
-    synchronize: true,
-    logging: false
-}));
+export default registerAs('typeorm', () => {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    return isProduction ? {
+        type: 'postgress',
+        url: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+        autoLoadEntities: true,
+        synchronize: true,
+    } : {
+        type: 'postgres',
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT ?? '', 10) || 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: false,
+    }
+});
 
 // export default registerAs('typeorm', () => config)
 // export const connectionSource = new DataSource(config)
